ed98eeb00c8e0eb520fd2bb3092ad17b1f079018
Appu-Srn <39372051+Appu-Srn@users.noreply.github.com>
2019-01-28 12:53:06 +0530

    second commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Rainbow from '../hoc/Rainbow';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About </h4>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi autem quia ipsa quos dolore explicabo, quisquam omnis voluptas nostrum facilis dolor, aut eum ea dolores quis libero officia ad, incidunt iure quam pariatur. Dolores, tempora numquam perspiciatis debitis soluta cumque nulla quisquam voluptatum beatae perferendis aliquid dolorem cupiditate eum delectus repellendus ab ipsa repellat pariatur quae totam quod earum eius! Quibusdam explicabo, expedita, incidunt beatae autem veniam impedit, commodi quod iure eius iste cum rem minima doloribus. Nulla officia consectetur vitae nemo blanditiis! Nihil culpa tempore eligendi sequi, maxime esse doloremque rem? Mollitia cum quae vitae molestiae eum ratione, tempore recusandae velit eos necessitatibus ad qui voluptatem impedit doloremque nam, inventore nobis quam deserunt sapiente expedita non aperiam, perferendis facilis architecto. Similique, sit alias. Quae, dolores cupiditate incidunt, fugiat sapiente iusto quidem aliquid optio dicta doloremque in ipsum veritatis error quis consequatur! Maiores repellat harum eveniet accusamus perferendis. Alias ut minus sint rem, modi quis sapiente omnis quas iste eum, a labore quibusdam dolor voluptates maxime quam. Ea quaerat eos ducimus. Dignissimos quae officia maxime aut pariatur quasi, ex, quod consequatur sit tempore minima ut fugiat cumque, magnam unde culpa voluptates! Corrupti voluptates necessitatibus soluta? Ratione, cumque magni? Deleniti nisi repellat repellendus eligendi ut, tempora dolores explicabo tempore obcaecati corporis, maiores asperiores enim, vero laudantium pariatur ullam! Earum necessitatibus facere accusantium at laboriosam inventore velit nobis porro reiciendis qui doloribus similique incidunt amet dolor eos dolorum totam, nisi nam quos perferendis quas. Facilis vel reiciendis error deserunt nobis sint, dolor vitae! Odit quam quisquam excepturi optio eaque tempore mollitia facilis.</p>
+        </div>
+    )
+}
+export default Rainbow(About);
